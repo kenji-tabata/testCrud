8cbac5dd1e72c85524c62dd30f25396aed961108
Leandro Kenji <locky.kenji@gmail.com>
2014-09-15 08:57:04 -0300

    Validador

--- models/Validador.class.php
+++ models/Validador.class.php
@@ -6,12 +6,13 @@
 class Validador {
 
     /**
-     * Verifica se o INPUT recebeu algum valor
+     * Verifica se o $input é vazio ou não
+     * 
      * @param type $input
      * @return boolean
      */
     function ehVazio($input) {
-        # Retorna TRUE caso o campo esteja vazio
+        # Se for vazio...
         if (!$input) {
             return true;
         } else {
@@ -19,9 +20,15 @@ class Validador {
         }
     }
 
+    /**
+     * Verifica se é um formato de CPF válido
+     * 
+     * @param type $input
+     * @return boolean
+     */
     function validaCPF($input) {
-        # Retorna TRUE caso o CPF esteja no formato incorreto
-        if (!preg_match("/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}$/", $input)) {
+        # Se casar...
+        if (preg_match("/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}$/", $input)) {
             return true;
         } else {
             return false;
@@ -35,16 +42,16 @@ class Validador {
         # Caso a quantidade de campos vazio seja 1 retorna a mensagem no singular
         if ($qtdVazio == 1) {
             return "O campo " . $inputVazio[0] . " não foi preenchido ou está preenchido de forma incorreta.";
-        } 
+        }
         # Caso a quantidade de campos vazios seja maior que 1 retorna a mensagem no plural
         else {
             $inputs = $inputVazio[0];
             for ($erros = 1; $erros < $qtdVazio; $erros++) {
-                
+
                 # Se o laço FOR for igual a quantidade de campos vazios -1, atribui o 'e' na concatenação do INPUTS
                 if ($erros == $qtdVazio - 1) {
                     $inputs = $inputs . " e " . $inputVazio[$erros];
-                } 
+                }
                 # Se o laço FOR for diferente a quantidade de campos vazios -1, atribui a ',' na concatenação do INPUTS
                 else {
                     $inputs = $inputs . ", " . $inputVazio[$erros];
